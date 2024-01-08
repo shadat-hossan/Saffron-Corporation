98fafad8427ae2d3ba1cb7102a290e8ed42f1f27
shadat-hossan <shadathossan3500@gmail.com>
2024-01-08 12:56:40 +0600

    All file is changed and updated

--- js/index.js
+++ js/index.js
@@ -29,18 +29,6 @@ document.addEventListener('scroll', function () {
   }
 });
 
-// slick slider custom option
-$().ready(function(){
-  $('.slick-carousel').slick({
-    arrows: false,
-    centerPadding: "0px",
-    dots: true,
-    slidesToShow: 1,
-    infinite: false
-  });
-});
-
-
 
 
 
